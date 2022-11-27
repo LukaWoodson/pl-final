f371bf3d69e8187d3b828f11b2020beebc645d6c
Luka Woodson <llwoodson@ualr.edu>
2022-11-27 02:32:32 -0600

    Reader contents of ppm file as text

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("image");
+
+// ---------- function given by developer.mozilla.org on FileReader API ---------------
+function previewFile() {
+  const content = document.querySelector(".content");
+  const [file] = document.querySelector("input[type=file]").files;
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      // this will then display a text file
+      content.innerText = reader.result;
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsText(file);
+  }
+}
+
+// ----------- FOR REFERENCE FROM developer.mozilla.org on Canvas API -----------------
+// function draw() {
+//   const canvas = document.getElementById("canvas");
+//   if (canvas.getContext) {
+//     const ctx = canvas.getContext("2d");
+//
+//     ctx.fillStyle = "rgb(200, 0, 0)";
+//     ctx.fillRect(10, 10, 50, 50);
+//
+//     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+//     ctx.fillRect(30, 30, 50, 50);
+//   }
+// }
