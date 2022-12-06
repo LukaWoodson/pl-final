5167d4ee9782ff87a6d5a7d5658e04549fbca008
Luka Woodson <llwoodson@ualr.edu>
2022-12-05 23:04:29 -0600

    Simple parse file into data object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
-// ---------- function given by developer.mozilla.org on FileReader API ---------------
+// ---------- function given by developer.mozilla.org on FileReader API (modified)---------------
 function previewFileAsText() {
   const content = document.querySelector(".content");
-  const [file] = document.querySelector("input[type=file]").files;
+  let [file] = document.querySelector("input[type=file]").files;
   const reader = new FileReader();
 
   reader.addEventListener(
@@ -9,6 +9,7 @@ function previewFileAsText() {
     () => {
       // this will then display a text file
       content.innerText = reader.result;
+      parseFile(reader.result);
     },
     false
   );
@@ -16,6 +17,47 @@ function previewFileAsText() {
   if (file) {
     reader.readAsText(file);
   }
+}
+
+function parseFile(fileText) {
+  // save an object split on every new line
+  let result = fileText.trim().replaceAll("\r", "").split("\n");
+  let data = {
+      fileType: "",
+      width: 0,
+      height: 0,
+      maxColorVal: 0,
+      colors: [],
+    },
+    rows = [];
+
+  // loop through the results array to parse the information we need
+  for (let i of result) {
+    // remove anything in the string after # so all comments are removed
+    let substring = i.split(/#.*$/);
+    for (let index of substring) {
+      if (index !== "") {
+        // push the parsed information onto the rows array
+        rows.push(index.trim());
+      }
+    }
+  }
+
+  // save fileType (P3), image width and height, and the max color value
+  data.fileType = rows[0];
+  [data.width, data.height] = rows.at(1).split(" ");
+  data.maxColorVal = rows.at(2);
+
+  //loop through remaining data and parse it into array of objects containing rgb values
+  for (let i = 3; i < rows.length; i++) {
+    let colorArr = rows[i].split(/\s+/);
+    data.colors.push({
+      r: colorArr[0],
+      g: colorArr[1],
+      b: colorArr[2],
+    });
+  }
+  console.log(data);
 }
 
 // ----------- FOR REFERENCE FROM developer.mozilla.org on Canvas API -----------------
