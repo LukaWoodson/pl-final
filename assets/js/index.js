f61073ac5842f2f757567f71a2134380554569c3
Luka Woodson <llwoodson@ualr.edu>
2022-12-07 16:28:04 -0600

    Refactor selectedPixel functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,23 +5,14 @@ import selectedPixel from "./selectedPixel.js";
 // ---------- function given by developer.mozilla.org on FileReader API (modified)---------------
 const input = document.querySelector("input[type=file]");
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
 const colorPicker = document.querySelector("input[type=color]");
 let image = null;
 
 input.addEventListener("change", handleFile);
 
-canvas.addEventListener("click", (e) => {
-	const {row, column} = handlePixelClick(e);
-	selectedPixel.updatePosition(row, column);
-	colorPicker.value = image.getPixel(row, column).getHex();
-});
+canvas.addEventListener("click", handleCanvasClick);
 
-colorPicker.addEventListener("change", (e) => {
-	const {row, column} = selectedPixel.getPosition();
-	image.updatePixelXY(row, column, Pixel.hexToRgb(e.target.value));
-	draw(context, image);
-});
+colorPicker.addEventListener("change", handleColorChange);
 
 function handleFile() {
 	const [file] = input.files;
@@ -35,7 +26,7 @@ function handleFile() {
 			canvas.width = image.getWidth();
 			canvas.height = image.getHeight();
 			selectedPixel.setDimensions(canvas.width, canvas.height);
-			draw(context, image);
+			draw();
 			selectedPixel.updatePosition(0, 0);
 		},
 		false
@@ -46,7 +37,8 @@ function handleFile() {
 	}
 }
 
-function draw(context, image) {
+function draw() {
+	const context = canvas.getContext("2d");
 	const imageData = new ImageData(
 		image.toUint8ClampedArray(),
 		canvas.width,
@@ -62,4 +54,16 @@ function handlePixelClick(event) {
 	const pxWidth = canvas.clientWidth / image.getWidth();
 	const pxHeight = canvas.clientHeight / image.getHeight();
 	return {column: Math.floor(x / pxWidth), row: Math.floor(y / pxHeight)};
+}
+
+function handleCanvasClick(event) {
+	const {row, column} = handlePixelClick(event);
+	selectedPixel.updatePosition(row, column);
+	colorPicker.value = image.getPixel(row, column).getHex();
+}
+
+function handleColorChange(event) {
+	const {row, column} = selectedPixel.getPosition();
+	image.updatePixelXY(row, column, Pixel.hexToRgb(event.target.value));
+	draw();
 }
